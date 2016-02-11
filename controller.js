3e04c82c036a139f7e9e093f45919c3372a53afa
Katie Slater <katiemyfriend@hotmail.com>
2016-02-11 15:21:28 -0700

    finished

---
+++ controller.js
@@ -0,0 +1,8 @@
+angular.module('userProfiles').controller('MainController', function($scope, mainService) {
+
+  $scope.getUsers = function() {
+    $scope.users = mainService.getUsers();
+  };
+  $scope.getUsers();
+
+});
